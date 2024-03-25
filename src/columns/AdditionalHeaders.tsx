2c1131f67c5ab9dc94f8d9f4ec102d10bcf7f06f
Sundar <sundar.b@biggby.com>
2024-03-25 21:28:52 +0530

    Column Header Customization

--- src/columns/AdditionalHeaders.tsx
+++ src/columns/AdditionalHeaders.tsx
@@ -1,9 +1,35 @@
-import { TableHeaderCell } from "@fluentui/react-components";
+import {
+  Menu,
+  MenuDivider,
+  MenuGroup,
+  MenuGroupHeader,
+  MenuItem,
+  MenuList,
+  MenuPopover,
+  MenuTrigger,
+  TableHeaderCell,
+} from "@fluentui/react-components";
 
 export const AdditionalHeaders: React.FC = () => {
   return (
-    <TableHeaderCell style={{ width: "28px" }} sortable={false}>
-      {"Actions"}
-    </TableHeaderCell>
+    <Menu>
+      <MenuTrigger disableButtonEnhancement>
+        <TableHeaderCell style={{ width: "28px" }} sortable={false}>
+          {"Actions"}
+        </TableHeaderCell>
+      </MenuTrigger>
+
+      <MenuPopover>
+        <MenuList>
+          <MenuGroup>
+          <MenuGroupHeader>Sort</MenuGroupHeader>
+            <MenuItem secondaryContent="A to Z">Asc</MenuItem>
+            <MenuItem secondaryContent="Z to A">Desc</MenuItem>
+          </MenuGroup>
+          <MenuDivider />
+          <MenuItem secondaryContent="Ctrl+O">Clear</MenuItem>
+        </MenuList>
+      </MenuPopover>
+    </Menu>
   );
 };
